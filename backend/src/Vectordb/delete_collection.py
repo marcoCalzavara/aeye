4f8e206f9e46aa08127b3c72c3f0a6bcc1baaae9
mcalzavara <mcalzavara@student.ethz.ch>
2023-11-02 10:00:21 +0100

    Added delete_collection.py

---
+++ backend/src/Vectordb/delete_collection.py
@@ -0,0 +1,48 @@
+import getpass
+import sys
+import typing
+
+from pymilvus import Collection
+from pymilvus import utility, connections, db
+
+from src.CONSTANTS import *
+
+
+def delete_collection(connection=False, passwd=None, collection_name=None) -> typing.Tuple[Collection, str]:
+    try:
+        if not connection and passwd is None:
+            raise Exception("Function requires either a password or a connection.")
+        # Create connection to Milvus server
+        if not connection:
+            connections.connect(
+                user=ROOT_USER,
+                password=passwd,
+                host=HOST,
+                port=PORT
+            )
+
+        # Create a database and switch to the newly created database
+        if DATABASE_NAME not in db.list_database():
+            db.create_database(DATABASE_NAME)
+        db.using_database(DATABASE_NAME)
+
+        print(f"Available collections: {utility.list_collections()}")
+
+        if collection_name is None:
+            collection_name = input("Choose collection name: ")
+
+        if collection_name not in utility.list_collections():
+            print("Collection does not exist.")
+        else:
+            print("Dropping collection...")
+            utility.drop_collection(collection_name)
+            print("Collection dropped.")
+
+    except Exception as e:
+        print(e.__str__())
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    passwd = getpass.getpass("Root password: ")
+    delete_collection(passwd=passwd)
