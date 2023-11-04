398a1da85818879294260b6fc592cf44d6a57493
mcalzavara <mcalzavara@student.ethz.ch>
2023-11-05 00:39:42 +0100

    Minor changes to docker-compose, backend Dockerfile and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
-echo "Building react application..."
+echo "Do you want to build the app? (y/n)"
+read -r answer
 
-cd frontend || exit
-npm run build
-
-# Navigate back to image-viz directory
-cd ..
+if [ "$answer" = "y" ]; then
+  echo "Building react application..."
+  cd frontend || exit
+  npm ci
+  npm run build
+  # Navigate back to image-viz directory
+  cd ..
+fi
 
 docker compose up -d
