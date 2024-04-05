ba637a30d4e6f41fafeec269e3789b0f427f5ab1
marcoCalzavara <mcalzavara@student.ethz.ch>
2024-04-05 14:31:20 +0200

    Attempt to change permissions

--- nginx/start.sh
+++ nginx/start.sh
@@ -6,6 +6,14 @@ chmod 644 /frontend/build
 find /frontend/build -type d -exec chmod 644 {} \;
 find /frontend/build -type f -exec chmod 644 {} \;
 
+chmod +rw /usr/share/nginx/best_artworks/
+chmod +rw /usr/share/nginx/celebahq/
+chmod +rw /usr/share/nginx/COCO-2017/
+chmod +rw /usr/share/nginx/Fashion-MNIST/
+chmod +rw /usr/share/nginx/MNIST/
+chmod +rw /usr/share/nginx/wikiart/
+chmod +rw /usr/share/nginx/CIFAR-100/
+
 sleep 115
 
 nginx -g 'daemon off;'
